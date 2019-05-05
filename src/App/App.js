b409f75e378767c1fcf2a7ad8b32914773a75d42
Nikhil Vijayan <nkhilv@gmail.com>
2019-05-05 01:06:59 +0100

    Add GlobalStyles to App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,11 @@
 import React, { Component, Fragment } from 'react';
+import GlobalStyles from './components/styled_components/GlobalStyles';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
+        <GlobalStyles />
         <h1>Hello World</h1>
       </Fragment>
     );
